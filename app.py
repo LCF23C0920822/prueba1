e289f30251de3e2b6b2fac53bc95757527629a20
Miguel Poma <miguelpoma2909@gmail.com>
2024-10-18 16:48:37 -0400

    cambiios

---
+++ app.py
@@ -0,0 +1,34 @@
+#Student ID: c0920822
+# Miguel Poma 
+from flask import Flask, render_template
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener valores de las variables de entorno
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+MONGODB_CLUSTER  = os.getenv('MONGODB_CLUSTER')
+
+app = Flask(__name__)
+
+# Conexión a MongoDB Atlas
+#client = MongoClient('mongodb+srv://mpoma:Universo789$@mycluster789.mdxep.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster789')
+client = MongoClient(f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName=MyCluster789')
+db = client.shop_db
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/products')
+def products():
+    products_collection = db.products
+    products = products_collection.find()
+    return render_template('products.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
